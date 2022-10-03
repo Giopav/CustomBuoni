5f869c19411c0bb716b347c09ac084d1fe79f173
Giopav <43653262+Giopav@users.noreply.github.com>
2022-10-03 22:28:44 +0200

    Added player.sendMessage warnings to the CommandHandler.

--- src/main/java/it/giopav/custombuoni/CommandHandler.java
+++ src/main/java/it/giopav/custombuoni/CommandHandler.java
@@ -1,11 +1,11 @@
 package it.giopav.custombuoni;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -13,13 +13,15 @@ import java.util.Objects;
 public class CommandHandler implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "The command can only be executed by a player!");
             return false;
         }
         Player player = (Player) sender;
         if (Objects.requireNonNull(((Player) sender).getPlayer()).getEquipment().getItemInMainHand().getType() != Material.PAPER) {
+            player.sendMessage(ChatColor.RED + "Your main hand does not have paper equipped!");
             return false;
         }
-        player.getInventory().addItem(new ItemStack(Material.DIAMOND));
+        player.sendMessage(ChatColor.RED + "Paper detected!");
         return true;
     }
 }
